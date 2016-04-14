a14dd93083368c23362f288df315fd0c7a51989f
Den4ikmal1 <denisreal725@gmail.com>
2016-04-14 22:31:20 +0300

    fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,12 @@
 class Ability
   include CanCan::Ability
 
-  attr_reader :user
 
   def initialize(user)
     @user = user
     
     if user 
-      user.admin? ? admin_attributes : user_attributes
+      user.admin? ? admin_attributes : user_attributes(user)
     else
      guest_attributes
     end
@@ -17,11 +16,11 @@ class Ability
     can :manage, :all
   end
 
-  def user_attributes
+  def user_attributes(user)
     can :read, :all
     can :create,  [Review]
-    can :updade,  [Review], user: user
-    can :destroy, [Review], user: user
+    can :updade,  [Review], user_id: user.id
+    can :destroy, [Review], user_id: user.id
     guest_attributes
   end
 
